804262f1bdf53ef067ad824209030939f1bc399d
bzzandrea <bzzandrea@gmail.com>
2015-05-12 14:48:13 +0200

    Revert "Testing some more with animation"
    This reverts commit dec8f64682920cc88106eb7419fe4ae07a4c7c5c.

--- Thepursuit/app/src/main/java/com/example/michael/ui/activities/LobbyActivity.java
+++ Thepursuit/app/src/main/java/com/example/michael/ui/activities/LobbyActivity.java
@@ -54,8 +54,7 @@ public class LobbyActivity extends ActionBarActivity {
         ButterKnife.inject(this);
         nickName = getIntent().getStringExtra("nickName");
         isLobbyLeader = getIntent().getBooleanExtra("isLobbyLeader", false);
-        lobbyGameCodeView.setText("Game code: " + getIntent().getStringExtra("gameID").toString() + "\n"
-                                    + "The duration of the game is: " + "\n" + "The radius of the game is: ");
+        lobbyGameCodeView.setText("Game code: " + getIntent().getStringExtra("gameID").toString());
         adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_checked, getIntent().getStringArrayListExtra("players")); // Ugly for now, doesn't show connected players at FIRST!
         playerList.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
